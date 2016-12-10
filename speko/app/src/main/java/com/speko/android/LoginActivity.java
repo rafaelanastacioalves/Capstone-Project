9851be82a7881d9d03b908ec808459874d4eb390
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-12-10 15:21:37 -0200

    Adiciona código de login pelo firebase

--- speko/app/src/main/java/com/speko/android/LoginActivity.java
+++ speko/app/src/main/java/com/speko/android/LoginActivity.java
@@ -1,13 +1,73 @@
 package com.speko.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            setContentView(R.layout.activity_login);
+
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                            .setProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                                    .build(),
+                    RC_SIGN_IN);
+        }
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                L
+                startActivity(new Intent(this, HomeActivity.class));
+                finish();
+                return;
+            }
+
+            // Sign in canceled
+            if (resultCode == RESULT_CANCELED) {
+                //TODO Implement
+//                showSnackbar(R.string.sign_in_cancelled);
+                return;
+            }
+
+            // No network
+            if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+                //TODO Implement
+//                showSnackbar(R.string.no_internet_connection);
+                return;
+            }
+
+            // User is not signed in. Maybe just wait for the user to press
+            // "sign in" again, or show a message.
+        }
+    }
+
+
+
 }
