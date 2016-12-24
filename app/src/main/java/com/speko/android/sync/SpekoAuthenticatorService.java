b6faa4d4559e94a4e4680f84bb2383d5459b4e66
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-12-24 10:17:39 -0300

    Put some log info

--- app/src/main/java/com/speko/android/sync/SpekoAuthenticatorService.java
+++ app/src/main/java/com/speko/android/sync/SpekoAuthenticatorService.java
@@ -3,6 +3,7 @@ package com.speko.android.sync;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by rafaelalves on 19/12/16.
@@ -13,11 +14,13 @@ public class SpekoAuthenticatorService extends Service {
 
     @Override
     public void onCreate() {
+        Log.d("AuthenticatorService", "onCreate");
         mAuthenticator = new SpekoAuthenticator(this);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d("AuthenticatorService", "onBind");
         return mAuthenticator.getIBinder();
     }
 }
