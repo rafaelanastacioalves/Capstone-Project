75080ee4c491014eb866d9067f93a59164b34cf6
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-12-26 12:11:55 -0300

    Create minimum domain entities

---
+++ app/src/main/java/com/speko/android/data/UserEntity.java
@@ -0,0 +1,23 @@
+package com.speko.android.data;
+
+/**
+ * Created by rafaelanastacioalves on 12/26/16.
+ */
+
+public class UserEntity {
+    private String name;
+    public UserEntity(){
+
+    }
+    public UserEntity(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
