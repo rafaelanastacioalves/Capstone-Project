80ba2ef67eb04097d71c9da473dd28acc134c61b
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-12-28 16:51:11 -0300

    Creates simple domain model classes

---
+++ app/src/main/java/com/speko/android/data/Friend.java
@@ -0,0 +1,9 @@
+package com.speko.android.data;
+
+/**
+ * Created by rafaelanastacioalves on 12/26/16.
+ */
+
+public class Friend {
+    private String name;
+}
---
+++ app/src/main/java/com/speko/android/data/User.java
@@ -0,0 +1,23 @@
+package com.speko.android.data;
+
+/**
+ * Created by rafaelanastacioalves on 12/26/16.
+ */
+
+public class User {
+    private String name;
+    public User(){
+
+    }
+    public User(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
