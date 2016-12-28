e8e1476f70124c815d4766f259b676f9d4fdaf0b
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-12-28 16:51:56 -0300

    Make first call to Firebase to get data via SyncAdapter

--- app/src/main/java/com/speko/android/sync/SpekoSyncAdapter.java
+++ app/src/main/java/com/speko/android/sync/SpekoSyncAdapter.java
@@ -9,6 +9,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.speko.android.data.User;
+import com.speko.android.retrofit.FirebaseClient;
+import com.speko.android.retrofit.ServiceGenerator;
+
+import java.io.IOException;
+
+import retrofit2.Call;
 
 
 /**
@@ -34,6 +41,23 @@ public class SpekoSyncAdapter extends AbstractThreadedSyncAdapter {
     @Override
     public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
         Log.d(LOG_TAG, "onPerformSync");
+
+        // Create a very simple REST adapter which points the GitHub API endpoint.
+        FirebaseClient client = ServiceGenerator.createService(FirebaseClient.class);
+
+        // Fetch and print a list of the contributors to this library.
+        Call<User> call = client.getUser("-K_0dp55MnCf5edl2J8M");
+
+        try {
+            User user = call.execute().body();
+            Log.i(LOG_TAG, "Deu certo!: \n" + user.getName());
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Deu ruim: \n" + e.getMessage());
+            // handle errors
+        }
+
+
     }
 
     public static void initializeSyncAdapter(){
